7e2b66f2516f4a98181c5b43ae341ccd75c481cf
0Teken6 <timu.rev006@gmail.com>
2024-06-07 00:01:05 +0600

    homework3.1

--- home/homeWork3.py
+++ home/homeWork3.py
@@ -1,30 +1,30 @@
 class Bank:
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
+        self._name = name
+        self._balance = balance
 
     def moneyX(self):
         money = int(input('Money to add: '))
-        self.balance += money
+        self._balance += money
 
     def _kill(self):
-        self.balance = 0
+        self._balance = 0
 
     def __jackpot(self):
-        self.balance *= 10
+        self._balance *= 10
 
     def _merge(self, another_balance):
-        self.balance += another_balance
+        self._balance += another_balance
 
-
+print(dir(Bank))
 b1 = Bank('Temirlan', 1000)
 b1.moneyX()
-print(b1.balance)
+print(b1._balance)
 b1._Bank__jackpot()
-print(b1.balance)
+print(b1._balance)
 b1._kill()
-print(b1.balance)
+print(b1._balance)
 b2 = Bank('Random', 12000)
-b1._merge(b2.balance)
-print(b1.balance)
+b1._merge(b2._balance)
+print(b1._balance)
 
